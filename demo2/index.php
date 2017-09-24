ba651c59546170a29629c8fd98e70bc0539bbb79
coyote91 <david.f.r91@hotmail.com>
2017-09-23 22:54:06 -0500

    añado dos modelos de breadcrumb sin codigo javascript

---
+++ demo2/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+  <link rel="stylesheet" href="./css/style.css">
+
+  </head>
+  <body>
+
+<!--  godaddy -->
+
+<div class="cart-header-wrap">
+       <div class="cart-page-header">
+           <div class="cart-steps">
+               <a class="cart-no-deco" data-ci="84292" href="https://cart.godaddy.com/Basket.aspx?isc=hos1co01">
+               <div class="cart-step-wrap cart-step-1">
+               <div class="cart-step-line-active"></div>
+               <div class="cart-step-complete">1</div>
+               <div class="cart-step-text-inactive">Carrito</div>
+               </div>
+               </a>
+
+               <a class="cart-no-deco" data-ci="84293" href="https://cart.godaddy.com/Checkout/Payment.aspx?isc=hos1co01" onclick="return false;">
+               <div class="cart-step-wrap cart-step-2">
+               <div class="cart-step-line-inactive"></div>
+               <div class="cart-step-active">2</div>
+               <div class="cart-step-text-active">Facturación y pago</div>
+               </div>
+               </a>
+
+               <div class="cart-step-wrap cart-step-3">
+               <div class="cart-step-inactive">3</div>
+               <div class="cart-step-text-inactive">Gracias</div>
+               </div>
+           </div>
+       </div>
+ </div>
+
+
+  </body>
+</html>
---
+++ demo3/index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+
+<div class="UploadProcess__container___xyAAl-">
+
+
+<div class="ContentAnimator__content___2mk9w- ContentAnimator__content-animated___2fKZb-">
+  <div class="UploadProcess__step-bar___2Ahg0-">
+    <div class="StepBar__step-bar___1uq7c-">
+
+           <!--
+                desactivado
+
+                StepBar__step-current___1Llc--
+            -->
+
+          <!---
+                activo
+
+                StepBar__step___2jzln-  StepBar__step-completed___1PxD3-
+          -->
+      <div class="StepBar__step___2jzln- StepBar__step-completed___1PxD3-">
+        <div class="StepBar__step-circle___1MAKk-">
+
+        </div>
+        <div>
+          <div class="StepBar__step-line___3v_H3-">
+
+          </div>
+          <!--
+          cuando este activo :
+
+             StepBar__step-line-progress___2cXYI- StepBar__progress-active___3kWXD-
+
+           --->
+
+           <!--
+
+          cuando no este activo :
+
+          StepBar__step-line-progress___2cXYI- StepBar__progress-not-active___3vxkm-
+
+           --->
+          <div class="StepBar__step-line-progress___2cXYI- StepBar__progress-active___3kWXD-">
+
+          </div>
+        </div>
+        <div class="StepBar__step-name___uKarv-">Price</div>
+      </div>
+                                      <!-- enciende el circulo -->
+      <div class="StepBar__step___2jzln- StepBar__step-current___1Llc-- StepBar__step-completed___1PxD3-"> <!-- añado aqui los nombre de clase  de lo que nombre abajo -->
+
+        <!--
+           cuando este activo
+
+        StepBar__step___2jzln- StepBar__step-current___1Llc-- StepBar__step-completed___1PxD3-
+
+        -->
+
+        <!-- cuando este desactivado
+
+         StepBar__step___2jzln- StepBar__step-waiting___1sYsb-
+
+        -->
+
+        <div class="StepBar__step-circle___1MAKk-">
+
+        </div>
+        <div>
+          <div class="StepBar__step-line___3v_H3-">
+
+          </div>
+          <div class="StepBar__step-line-progress___2cXYI- StepBar__progress-not-active___3vxkm-">
+
+          </div>
+        </div>
+        <div class="StepBar__step-name___uKarv-">Details</div>
+      </div>
+      <div class="StepBar__step___2jzln- StepBar__step-waiting___1sYsb-">
+        <div class="StepBar__step-circle___1MAKk-">
+
+        </div>
+        <div>
+          <div class="StepBar__step-line___3v_H3-">
+
+          </div>
+          <div class="StepBar__step-line-progress___2cXYI- StepBar__progress-not-active___3vxkm-">
+
+          </div>
+        </div>
+        <div class="StepBar__step-name___uKarv-">Account</div>
+      </div>
+      <div class="StepBar__step___2jzln- StepBar__step-waiting___1sYsb-">
+        <div class="StepBar__step-circle___1MAKk-">
+
+        </div>
+        <div class="StepBar__step-name___uKarv-">Distribute</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+  </body>
+</html>
