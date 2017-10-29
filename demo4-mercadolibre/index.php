20cd8e994040635086ae3afa6a7dcc5fa2cb91db
coyote91 <david.f.r91@hotmail.com>
2017-10-29 17:12:24 -0500

    Añado ejercicio 4 y 5

---
+++ demo4-mercadolibre/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+
+
+     <div class="navmaincontent">
+
+         <div class="ch-box-container">
+
+              <form class="" action="?execution=e1s1" method="post">
+
+                <input name="execution" value="e1s1" id="execution" type="hidden">
+
+                <ol class="ch-wizard-breadcrumb ch-steps-five">
+
+                   <li class="ch-wizard-current">Elige qué quieres publicar</li>
+                   <li class="ch-wizard-step">Describe tu producto</li>
+                   <li class="ch-wizard-step">Ingresa precio y cantidad</li>
+                   <li class="ch-wizard-step">Elige un tipo de publicación</li>
+                   <li class="ch-wizard-step">Regí­strate y confirma</li>
+
+               </ol>
+
+
+
+
+              </form>
+
+         </div>
+
+     </div>
+
+  </body>
+</html>
---
+++ demo5/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+
+    <!-- a simple div with some links -->
+  <div class="breadcrumb">
+  	<a href="#" class="active">Browse</a>
+  	<a href="#">Compare</a>
+  	<a href="#">Order Confirmation</a>
+  	<a href="#">Checkout</a>
+  </div>
+  <br /><br />
+  <!-- another version - flat style with animated hover effect -->
+  <div class="breadcrumb flat">
+  	<a href="#" class="active">Browse</a>
+  	<a href="#">Compare</a>
+  	<a href="#">Order Confirmation</a>
+  	<a href="#">Checkout</a>
+  </div>
+
+
+  </body>
+</html>
